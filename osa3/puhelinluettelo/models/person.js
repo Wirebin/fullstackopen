950972cdf6e4477bc7ff0a8f9b12e45fd7494d25
Wirebin <107724483+Wirebin@users.noreply.github.com>
2025-07-28 15:13:38 +0300

    Fix linter issues

--- osa3/puhelinluettelo/models/person.js
+++ osa3/puhelinluettelo/models/person.js
@@ -6,38 +6,38 @@ const url = process.env.MONGODB_URI
 
 console.log(`Connecting to ${url}`)
 mongoose.connect(url)
-	.then(() => {
-		console.log('Connected to MongoDB')
-	})
-	.catch(error => {
-		console.log(`Failed to connect to MongoDB, ${error.message}`)
-	})
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(error => {
+    console.log(`Failed to connect to MongoDB, ${error.message}`)
+  })
 
 const personSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		minlength: 3,
-		required: true
-	},
-	number: {
-		type: String,
-		minlength: 8,
-		validate: {
-			validator: function(v) {
-				return /\d{2}-\d{6,}|\d{3}-\d{5,}/.test(v)
-			},
-			message: props => `${props.value} is not a valid phone number!`
-		},
-		required: true
-	},
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{2}-\d{6,}|\d{3}-\d{5,}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: true
+  },
 })
 
 personSchema.set('toJSON', {
-	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString()
-		delete returnedObject._id
-		delete returnedObject.__v
-	}
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
